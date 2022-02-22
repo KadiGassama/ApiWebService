49bd69e02b355e6834e76085b6df8a65d66d6acd
kadi <kadijetou.gassama@epsi.fr>
2022-02-22 10:03:28 +0100

    Initial commit

---
+++ demo/src/main/java/com/example/demo/controllers/helloWorldController.java
@@ -0,0 +1,18 @@
+package com.java.springboothelloworld.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloWorldController {
+
+    @GetMapping(
+            path = "/hello",
+            produces = "text/plain"
+    )
+    public String getHelloWorld(){
+        return "Hello Woooorrrlldd !";
+    }
+
+
+}
